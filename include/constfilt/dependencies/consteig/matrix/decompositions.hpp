3999f5d96fe6fa7d60b9f67e96d07d1c2c4ba3f2
Mitchell Thompkins <mitchell.thompkins@pm.me>
2026-04-19 03:04:18 +0000

    Revert "add in deps"
    This reverts commit 5f40530a7152927d5e5b83a87461d6f9bc749182.

--- include/constfilt/dependencies/consteig/matrix/decompositions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DECOMPOSITIONS_HPP
-#define DECOMPOSITIONS_HPP
-
-#include "decompositions/hessenberg.hpp"
-#include "decompositions/householder.hpp"
-#include "decompositions/lu.hpp"
-#include "decompositions/qr.hpp"
-
-#endif
